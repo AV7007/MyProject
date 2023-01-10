852682cc732cabf8fbdfc690a256649b7a421337
av7007 <githubidofav@gmail.com>
2023-01-10 17:00:03 +0530

    My First Git Practice

---
+++ src/org/Constructor/Constructor_Test.java
@@ -0,0 +1,52 @@
+package org.Constructor;
+
+public class Constructor_Test {
+
+	
+	public static void main(String[] args) {
+
+		String s = "AntoVargheese";
+		int vowelCount = 0;
+		String vowels = "";
+		int consonentCount = 0 ;
+		String consonents = "";
+		
+		for (int i = 0; i < s.length(); i++) {
+			
+			char c = s.charAt(i);
+			
+			if (c==65||c==69||c==73||c==79||c==85||c==97||c==101||c==105||c==111||c==117) {
+				
+				vowels = vowels+c;
+				
+				
+				
+				vowelCount++;
+			
+				}
+			
+			else {
+				
+			
+				consonents = consonents + c;
+				consonentCount++;
+				
+				
+			}
+		}
+		System.out.println("Vowels Present 		:" + vowels);
+		System.out.println("Vowels Count 		:" + vowelCount);
+		System.out.println("Consonent Count 	:" + consonentCount);
+		System.out.println("Consonents Present 	:" + consonents);
+		
+
+	}
+
+
+
+	
+
+
+}
+
+
